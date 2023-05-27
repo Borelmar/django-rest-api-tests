231c6685129ab253441c5b224a4a093f3013b533
Borelmar <Ctipax@gmail.com>
2023-05-27 20:05:50 +0300

    remove backup

--- test_backup/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
-
-class IsTutorOrAdmin(BasePermission):
-    def has_permission(self, request, view):
-        if request.user.role == 'tutor' or request.user.is_staff:
-            return True
